03997733ee2a662ba1f2906585055fc3f890b34b
Naod Tadele <62254352+kronahs@users.noreply.github.com>
2023-02-11 19:41:07 +0300

    Add project files.

---
+++ chatGPT/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace chatGPT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnBody = new System.Windows.Forms.Panel();
+            this.txtPrompt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbResponse = new System.Windows.Forms.RichTextBox();
+            this.pbResponse = new Krypton.Toolkit.KryptonProgressBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnBody.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.78067F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.943F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.15242F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pnBody, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblHint, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pbResponse, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.2164F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.02443F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.933682F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(813, 630);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::chatGPT.Properties.Resources.ChatGPT_logo_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Poppins", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(90, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(628, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ChatGPT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnBody
+            // 
+            this.pnBody.Controls.Add(this.tableLayoutPanel2);
+            this.pnBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnBody.Location = new System.Drawing.Point(90, 79);
+            this.pnBody.Name = "pnBody";
+            this.pnBody.Size = new System.Drawing.Size(628, 509);
+            this.pnBody.TabIndex = 2;
+            // 
+            // txtPrompt
+            // 
+            this.txtPrompt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.txtPrompt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrompt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPrompt.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrompt.ForeColor = System.Drawing.SystemColors.Menu;
+            this.txtPrompt.Location = new System.Drawing.Point(3, 3);
+            this.txtPrompt.Multiline = true;
+            this.txtPrompt.Name = "txtPrompt";
+            this.txtPrompt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPrompt.Size = new System.Drawing.Size(564, 41);
+            this.txtPrompt.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(573, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90.85072F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.149278F));
+            this.tableLayoutPanel2.Controls.Add(this.txtPrompt, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.345795F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.65421F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(628, 509);
+            this.tableLayoutPanel2.TabIndex = 3;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHint.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblHint.Location = new System.Drawing.Point(724, 591);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(86, 39);
+            this.lblHint.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.tableLayoutPanel2.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.tableLayoutPanel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 456);
+            this.panel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.020101F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95.9799F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.rtbResponse, 1, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.952569F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 96.04743F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(622, 456);
+            this.tableLayoutPanel3.TabIndex = 0;
+            this.tableLayoutPanel3.DoubleClick += new System.EventHandler(this.copyText);
+            // 
+            // rtbResponse
+            // 
+            this.rtbResponse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.rtbResponse.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbResponse.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rtbResponse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbResponse.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbResponse.ForeColor = System.Drawing.SystemColors.Menu;
+            this.rtbResponse.Location = new System.Drawing.Point(27, 20);
+            this.rtbResponse.Name = "rtbResponse";
+            this.rtbResponse.ReadOnly = true;
+            this.rtbResponse.Size = new System.Drawing.Size(571, 415);
+            this.rtbResponse.TabIndex = 3;
+            this.rtbResponse.Text = "";
+            this.rtbResponse.DoubleClick += new System.EventHandler(this.copyText);
+            // 
+            // pbResponse
+            // 
+            this.pbResponse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbResponse.Location = new System.Drawing.Point(90, 594);
+            this.pbResponse.Name = "pbResponse";
+            this.pbResponse.Size = new System.Drawing.Size(628, 33);
+            this.pbResponse.TabIndex = 4;
+            this.pbResponse.UseKrypton = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(813, 630);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "ChatGPT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnBody.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnBody;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox txtPrompt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.RichTextBox rtbResponse;
+        private Krypton.Toolkit.KryptonProgressBar pbResponse;
+    }
+}
+
---
+++ chatGPT/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatGPT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pbResponse.Visible = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Define the prompt
+            
+        }
+
+        static string GenerateResponse(string apiEndpoint, string apiKey, string prompt)
+        {
+            using (var client = new HttpClient())
+            {
+                // Set the API key in the request header
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
+
+                // Define the request body
+                var requestBody = new StringContent(JsonConvert.SerializeObject(new
+                {
+                    prompt = prompt,
+                    max_tokens = 1024,
+                    n = 1,
+                    temperature = 0.5
+                }), Encoding.UTF8, "application/json");
+
+                // Send the API request
+                var response = client.PostAsync(apiEndpoint, requestBody).Result;
+                response.EnsureSuccessStatusCode();
+
+                // Get the response body
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                // Parse the response body
+                dynamic responseJson = JsonConvert.DeserializeObject(responseBody);
+
+                // Return the generated response
+                return responseJson["choices"][0]["text"];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pbResponse.Visible = true;
+            pbResponse.Minimum = 0;
+            pbResponse.Maximum = 100;
+
+            // Start the progress bar
+            pbResponse.Value = 0;
+            pbResponse.Step = 40;
+
+
+            string apiEndpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+            string apiKey = "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";
+
+            string prompt = txtPrompt.Text;
+
+            // Use the API to generate a response
+            string response = GenerateResponse(apiEndpoint, apiKey, prompt);
+
+            // Print the generated response
+           
+            while(rtbResponse.Text != response)
+            {
+                pbResponse.PerformStep();
+                rtbResponse.Text = response;
+            }
+             if(rtbResponse.Text != "")
+            {
+                pbResponse.Visible = false;
+            }
+
+            
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private  async void copyText(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(rtbResponse.Text);
+            lblHint.Text = "Text Copied";
+             await System.Threading.Tasks.Task.Delay(6000);
+            lblHint.Text = "";
+        }
+    }
+}
